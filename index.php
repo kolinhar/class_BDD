e749c9bed862f615cf54a92ad3429accf9631547
Kolinhar <kolinhar@users.noreply.github.com>
2014-09-01 09:47:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,43 +2,44 @@
     <head>
         <meta charset="UTF-8">
         <style>
-            body{
+            body {
                 background-color: #000;
                 color: #fff;
             }
         </style>
     </head>
     <body>
-<?php
-/**
- * Created by PhpStorm.
- * User: juanes
- * Date: 19/07/14
- * Time: 22:31
- */
-include "class_BDD.php";
+        <fieldset>
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: juanes
+         * Date: 19/07/14
+         * Time: 22:31
+         */
+        include "class_BDD.php";
 
-echo "<p>instanciation sans paramètre:</p>";
-$oBDD = new BDD;
-$oBDD->DataConnection("root", "","mysql:host=localhost;dbname=freeh_utopia");
-$oBDD->RqtSelect("select * from feuilles order by id_perso asc", []);
-$oBDD->NextLine();
-?>
-        <pre>
+        echo "<legend>instanciation sans paramètre:</legend>";
+        $oBDD = new BDD();
+        $oBDD->DataConnection("root", "", "mysql:host=localhost;dbname=freeh_utopia");
+        $oBDD->RqtSelect("select * from feuilles order by id_perso asc", []);
+        $oBDD->NextLine();
+        ?>
+
 <?php
-echo "1er ID trouvé : ".$oBDD->Valeur("id_perso");
+echo "1er ID trouvé : " . $oBDD->Valeur("id_perso");
 ?>
-        </pre>
+        </fieldset>
+        <fieldset>
+        <?php
+        echo "<legend>instanciation avec paramètres:</legend>";
+        $oBDD2 = new BDD("root", "", "mysql:host=localhost;dbname=freeh_utopia");
+        $oBDD2->RqtSelect("select * from feuilles order by id_perso asc", []);
+        $oBDD2->NextLine();
+        ?>
 <?php
-echo "<p>instanciation avec paramètre:</p>";
-$oBDD2 = new BDD("root", "","mysql:host=localhost;dbname=freeh_utopia");
-$oBDD2->RqtSelect("select * from feuilles order by id_perso asc", []);
-$oBDD2->NextLine();
+echo "1er ID trouvé : " . $oBDD2->Valeur("id_perso");
 ?>
-        <pre>
-<?php
-echo "1er ID trouvé : ".$oBDD2->Valeur("id_perso");
-?>
-        </pre>
+        </fieldset>
     </body>
 </html>
